ec483cb1c27997def9d69c9a3c565c1f4d90779b
WolfgangMatyus <wi21b055@technikum-wien.at>
2022-06-15 15:02:41 +0200

    UptimeControllerTest implementation

--- src/main/java/at/matyus/UptimeController.java
+++ src/main/java/at/matyus/UptimeController.java
@@ -1,7 +1,12 @@
 package at.matyus;
 
 public class UptimeController {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+
+    public String calculateUptime(){
+        double minutesOfMonth = 60*24*30;
+        double definedUptimeInPercent = 99.95/100;
+        double uptimeInMinutes = minutesOfMonth * definedUptimeInPercent;
+        return String.format("%.2f", uptimeInMinutes);
     }
+
 }
---
+++ src/test/java/at/matyus/UptimeControllerTest.java
@@ -0,0 +1,10 @@
+package at.matyus;
+
+import org.junit.jupiter.api.Test;
+
+public class UptimeControllerTest {
+    @Test
+    public void test_calculateUptime_shouldBeCalculatedCorrectly(){
+
+    }
+}
